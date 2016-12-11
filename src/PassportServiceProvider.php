d2ae1e5c20b842b51ea79648c1624717ee0dd8a9
xjstratedgebx <jarret@solvomedia.com>
2016-12-10 23:39:40 -0500

    Overload provider boot method for new ClientCommand

--- src/PassportServiceProvider.php
+++ src/PassportServiceProvider.php
@@ -8,6 +8,36 @@ use League\OAuth2\Server\Grant\PasswordGrant;
 class PassportServiceProvider extends ServiceProvider
 {
     /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../../laravel/passport/resources/views', 'passport');
+
+        $this->deleteCookieOnLogout();
+
+        if ($this->app->runningInConsole()) {
+            $this->registerMigrations();
+
+            $this->publishes([
+                __DIR__.'/../../laravel/passport/resources/views' => base_path('resources/views/vendor/passport'),
+            ], 'passport-views');
+
+            $this->publishes([
+                __DIR__.'/../../laravel/passport/resources/assets/js/components' => base_path('resources/assets/js/components/passport'),
+            ], 'passport-components');
+
+            $this->commands([
+                \Laravel\Passport\Console\InstallCommand::class,
+                Console\ClientCommand::class,
+                \Laravel\Passport\Console\KeysCommand::class,
+            ]);
+        }
+    }
+
+    /**
      * Register Passport's migration files.
      *
      * @return void
