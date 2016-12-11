ca35b0a9bd2daa8437a43012c8a869c4fd49b5ea
xjstratedgebx <jarret@solvomedia.com>
2016-12-10 19:12:00 -0500

    Register new migration files

---
+++ src/PassportServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dropwire\Passport;
+
+use League\OAuth2\Server\Grant\PasswordGrant;
+
+class PassportServiceProvider extends ServiceProvider
+{
+    /**
+     * Register Passport's migration files.
+     *
+     * @return void
+     */
+    protected function registerMigrations()
+    {
+        if (Passport::$runsMigrations) {
+            return $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        }
+
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'passport-migrations');
+    }
+}
