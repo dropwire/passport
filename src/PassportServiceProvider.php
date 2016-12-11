40ddbeb437cc29d467c88389ac05ec73404b72ab
xjstratedgebx <jarret@solvomedia.com>
2016-12-11 15:50:29 -0500

    Use the container to register overloaded ClientRepository

--- src/PassportServiceProvider.php
+++ src/PassportServiceProvider.php
@@ -9,36 +9,6 @@ use League\OAuth2\Server\Grant\PasswordGrant;
 class PassportServiceProvider extends BaseServiceProvider
 {
     /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->loadViewsFrom(__DIR__.'/../../laravel/passport/resources/views', 'passport');
-
-        $this->deleteCookieOnLogout();
-
-        if ($this->app->runningInConsole()) {
-            $this->registerMigrations();
-
-            $this->publishes([
-                __DIR__.'/../../laravel/passport/resources/views' => base_path('resources/views/vendor/passport'),
-            ], 'passport-views');
-
-            $this->publishes([
-                __DIR__.'/../../laravel/passport/resources/assets/js/components' => base_path('resources/assets/js/components/passport'),
-            ], 'passport-components');
-
-            $this->commands([
-                \Laravel\Passport\Console\InstallCommand::class,
-                Console\ClientCommand::class,
-                \Laravel\Passport\Console\KeysCommand::class,
-            ]);
-        }
-    }
-
-    /**
      * Register Passport's migration files.
      *
      * @return void
@@ -52,5 +22,34 @@ class PassportServiceProvider extends BaseServiceProvider
         $this->publishes([
             __DIR__.'/../database/migrations' => database_path('migrations'),
         ], 'passport-migrations');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerOverloads();
+
+        $this->registerAuthorizationServer();
+
+        $this->registerResourceServer();
+
+        $this->registerGuard();
+    }
+
+    /**
+     * Register overloaded classes.
+     *
+     * @return void
+     */
+    public function registerOverloads()
+    {
+        $this->app->bind(
+            \Laravel\Passport\ClientRepository::class,
+            \Dropwire\Passport\ClientRepository::class
+        );
     }
 }
